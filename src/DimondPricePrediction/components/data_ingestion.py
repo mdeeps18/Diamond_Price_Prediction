980b026f0fc294876ceceab5508b4240ded0ab2b
Dipali <mdeeps18@gmail.com>
2023-11-17 10:57:38 +0530

    new code

---
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+from src.DimondPricePrediction.logger import logging
+from src.DimondPricePrediction.exception import customexception
+
+import os
+import sys
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pathlib import Path
+
+class DataIngestion:
+    def __init__(self):
+        pass
+    
+    def initiate_data_ingestion(self):
+        pass
+    
+    
---
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -0,0 +1,10 @@
+
+
+
+
+class DataTransformation:
+    def __init__(self):
+        pass
+    
+    def get_data_transormation(self):
+        pass
