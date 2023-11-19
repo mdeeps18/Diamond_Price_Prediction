78c54a2216d21086907aae7d20f956570c2b26ca
Dipali <mdeeps18@gmail.com>
2023-11-19 20:04:30 +0530

    app.py updated

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-import os
+'''import os
 path="notebooks/EDA.ipynb"
 
-print(os.path.split(path))
+dir,file=os.path.split(path)
+os.makedirs(dir, exist_ok=True)
+
+with open(path,'w') as f:
+    pass'''
+
+from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData
+
+custdataobj=CustomData(0.34,62.0,54.0,4.47,4.5,2.78,"Ideal","G","VVS1")
+data=custdataobj.get_data_as_dataframe()
+
+print(data)
